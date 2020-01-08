fa1774351efd92c3155a5cf4ef08091ff484128f
NamHyun Gu <mnhan0403@gmail.com>
2020-01-08 19:12:39 +0900

    Updates

---
+++ precache-manifest.82586af73de4f8726e8827f2ba27ee62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9c60fd9559664238cbd0aa9ce88822",
+    "url": "/react-news-viewer/index.html"
+  },
+  {
+    "revision": "3869ee0ba827d9b87dff",
+    "url": "/react-news-viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b92d6b529431faee459a",
+    "url": "/react-news-viewer/static/js/2.912be348.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/react-news-viewer/static/js/2.912be348.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3869ee0ba827d9b87dff",
+    "url": "/react-news-viewer/static/js/main.514b8cdc.chunk.js"
+  },
+  {
+    "revision": "37a534a5b3ffd151c2fa",
+    "url": "/react-news-viewer/static/js/runtime-main.7ab3abf0.js"
+  }
+]);
