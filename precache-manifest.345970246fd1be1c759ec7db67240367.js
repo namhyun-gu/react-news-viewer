ee65503c9582563425eb3b4439ab5706da777f9c
namhyun-gu <mnhan0403@gmail.com>
2020-01-09 03:13:47 +0000

    Deploying to gh-pages from  aefd5767db9218ef9689d93cf7a3278f3f0055a8

---
+++ precache-manifest.345970246fd1be1c759ec7db67240367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b12c9066a9a002dd56604538d60b1ff",
+    "url": "/react-news-viewer/index.html"
+  },
+  {
+    "revision": "2b1abcfb32420dc2adbe",
+    "url": "/react-news-viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e3f0097c19260e042bb3",
+    "url": "/react-news-viewer/static/js/2.d13cc4df.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-news-viewer/static/js/2.d13cc4df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b1abcfb32420dc2adbe",
+    "url": "/react-news-viewer/static/js/main.dee787ad.chunk.js"
+  },
+  {
+    "revision": "55102da1c4faa3a918d4",
+    "url": "/react-news-viewer/static/js/runtime-main.39d174eb.js"
+  }
+]);
